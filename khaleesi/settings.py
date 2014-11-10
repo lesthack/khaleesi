0e9f153d818308e96e01246e6b6c0854d6b02d73
Jorge Luis Hernández <jhernandez@maices.com>
2014-11-10 12:04:33 -0600

    Pseudo relase.

--- khaleesi/settings.py
+++ khaleesi/settings.py
@@ -9,24 +9,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
-from sensible import *
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
-# Application definition
+SECRET_KEY = 'g2d!%60junl6-^uw4t7w1hotyljdh0@)fw9ymgyxtgleqs)=t='
+ 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 INSTALLED_APPS = (
     'suit',
@@ -55,24 +54,14 @@ TEMPLATE_CONTEXT_PROCESSORS = TCP + (
 )
 
 ROOT_URLCONF = 'khaleesi.urls'
-
 WSGI_APPLICATION = 'khaleesi.wsgi.application'
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = BASE_DIR + '/static'
 STATIC_URL = '/static/'
 
