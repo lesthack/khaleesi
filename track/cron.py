64d99fc8e82af09703b2755522a0291581fa13ec
Salvador <salvador@Salvador-Linux.(none)>
2015-02-13 10:46:18 -0600

    Corrección envío de correos diarios

--- track/cron.py
+++ track/cron.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 def mail_daily():
     hoy = datetime.now()
-    c = cita()
+    q = cita()
     date_exceptions = ['31/12/2014']
     list_users = UserProfile.objects.filter(is_email_active = 0)
 
@@ -18,7 +18,7 @@ def mail_daily():
             c = Context({
                 'hoy': hoy,
                 'user': user,
-                'quote': c.cita_aleatoria().descripcion,
+                'quote': q.cita_aleatoria().descripcion,
             })
             html_template = get_template('email_daily.html')
             html_content = html_template.render(c)
