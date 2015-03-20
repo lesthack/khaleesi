b75facbf2cc7704d053285d8bc33bb66c33c9c29
Jorge Luis Hernández <lesthack@gmail.com>
2015-03-20 17:30:14 -0600

    :bug: Fix. los urls prefería las de admin.

--- khaleesi/urls.py
+++ khaleesi/urls.py
@@ -19,7 +19,6 @@ v1_api.register(UserResource())
 urlpatterns = patterns('',
     url(r'^$', RedirectView.as_view(url='/admin')),
     url(r'^api/', include(v1_api.urls)),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/gantt/$', site.admin_view(gantt_all)),
     url(r'^admin/json/board/$', site.admin_view(json_board)),
     url(r'^admin/auth/profile/$', site.admin_view(user_profile)),
@@ -27,4 +26,5 @@ urlpatterns = patterns('',
     url(r'^admin/track/user/(?P<user_id>\d+)/gantt/$', site.admin_view(gantt_por_usuario)),
     url(r'^admin/track/user/(?P<user_id>\d+)/proyecto/(?P<proyecto_id>\d+)/gantt/$', site.admin_view(gantt_por_usuario_proyecto)),
     url(r'^admin/track/tarea/(?P<tarea_id>\d+)/board/(?P<status_id>\d+)/$', site.admin_view(board)),
+    url(r'^admin/', include(admin.site.urls)),
 )
