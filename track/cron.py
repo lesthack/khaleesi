0e33c98497a11fcd20d316d2e076d8a3f95d0c41
Jorge Luis Hernández <jhernandez@maices.com>
2015-04-27 14:49:14 -0500

    El filtro de usuarios que reciben mail estaban erroneos, provocando el envio de mas de uno por día para algunos usuarios.

--- track/cron.py
+++ track/cron.py
@@ -9,16 +9,15 @@ def mail_daily():
     hoy = datetime.now()
     q = cita()
     date_exceptions = ['31/12/2014']
-    list_users = UserProfile.objects.filter(is_email_active = 0)
 
     if hoy.weekday() not in [0,1,2,3,4] or hoy.strftime('%d/%m/%Y') in date_exceptions:
         return False
 
-    for user in User.objects.exclude(id__in=(l.user_id for l in list_users)):
+    for profile in UserProfile.objects.filter(is_email_active=True):
         try:
             c = Context({
                 'hoy': hoy,
-                'user': user,
+                'user': profile.user,
                 'quote': cita.objects.filter(deleted=False).order_by('?')[0],
                 'URL_HOST': URL_HOST
             })
@@ -27,7 +26,7 @@ def mail_daily():
             new_mail = mail()
             new_mail.subject = 'Actividades {}'.format(hoy.strftime('%B %d, %Y'))
             new_mail.body = html_content
-            new_mail.send_to = user
+            new_mail.send_to = profile.user
             new_mail.save()
         except Exception, e:
             print 'Error: ',e
