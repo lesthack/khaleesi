8ff897f6114d5919b12a585596d8eedc2d64e85b
Jorge Hernández <lesthack@users.noreply.github.com>
2019-03-11 11:30:44 -0600

    Considerando tareas no cerradas.

--- sql/functions/fnMinutosTarea.sql
+++ sql/functions/fnMinutosTarea.sql
@@ -1,10 +1,9 @@
 DROP FUNCTION IF EXISTS fnMinutosTarea;
-
-CREATE FUNCTION fnMinutosTarea(in_tarea_id INT, ini datetime, fin datetime) 
+CREATE FUNCTION fnMinutosTarea(in_tarea_id INT, ini datetime, fin datetime)
 RETURNS FLOAT
 BEGIN
     /*
-        Obtiene las horas reales de una tarea en especific
+        Obtiene las horas reales de una tarea
         entre una rango de fechas especificadas  
     */
 	DECLARE done INT DEFAULT FALSE;
@@ -16,11 +15,11 @@ BEGIN
     DECLARE segundos_totales FLOAT;
     
     DECLARE curs_tareas CURSOR FOR 
-        SELECT status, created_at 
+        SELECT status, convert_tz(track_pizarron.created_at, '+00:00', '-06:00') AS fecha 
         FROM track_pizarron 
         WHERE 
             track_pizarron.tarea_id = in_tarea_id
-            AND track_pizarron.created_at BETWEEN ini AND fin
+            AND convert_tz(track_pizarron.created_at, '+00:00', '-06:00') BETWEEN ini AND fin -- Se consideran 6 horas de diferencia
         ORDER BY track_pizarron.created_at;
     
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
@@ -52,10 +51,14 @@ BEGIN
         SET @last_sts := sts;
     END LOOP;   
     
+    -- Agregando opcion para tareas no finalizadas
+    -- Se considera límite a las 6 pm
+    IF @last_sts = 2 AND TIME(@fecha_ini) <= '18:00:00' THEN
+        -- Si el ultimo estatus es pausado
+        SET @segundos_totales = @segundos_totales + TIMESTAMPDIFF(SECOND, @fecha_ini, DATE_ADD(CAST(DATE(@fecha_ini) AS datetime), INTERVAL 1080 minute));
+    END IF;
+
     CLOSE curs_tareas;
     
     RETURN ceil(@segundos_totales/60);
 END;
-
-
--- SELECT fnMinutosTarea(420, CAST(DATE(NOW()) AS datetime), DATE_ADD(CAST(DATE(NOW()) AS datetime), INTERVAL 1439 minute))  
